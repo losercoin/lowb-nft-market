3439089c401343b623cd9f443de74da86e76fe00
tylerfyu <tylerfyu@outlook.com>
2022-02-08 03:06:13 -0800

    :bug: Fix: fix path public in config

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-	  publicPath: 'https://www.losernft.org/',
+	  publicPath: '/',
     clean: true,
   },
 });
